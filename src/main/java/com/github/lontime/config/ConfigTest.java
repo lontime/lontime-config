019160f749e5c1acfa0ad0cc754036a71b0be2e2
lontime <lontime2021@163.com>
2021-03-01 14:42:29 +0800

    init

---
+++ src/main/java/com/github/lontime/config/ConfigTest.java
@@ -0,0 +1,20 @@
+package com.github.lontime.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test.
+ * @author lontime
+ * @since 1.0
+ */
+public class ConfigTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigTest.class);
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
